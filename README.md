c4f4bd8c8932c65c1d38963ddbc42a98a3f54024
Pawel Ksieniewicz <pawel@ksienie.com>
2017-07-11 14:44:21 +0200

    d

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Abstract
+> Recently, the problem of imbalanced data is the focus of intense research of machine learning community. Following work tries to utilize an approach of transforming the data space into another where classification task may become easier. Paper contains a proposition of a tool, based on a photographic metaphor to build a classifier ensemble, combined with a random subspace approach. It is a naturally naive parallel, insensitive to a sample size, robust to dimension increase, which allows a regularization of feature space, to reduce the impact of biased classes. The proposed approach was evaluated on the basis of the computer experiments carried out on the benchmark datasets.
 # Hello!
 
 .  | 4.0 | 8.0 
--- docs.py
+++ docs.py
@@ -2,6 +2,10 @@
 import numpy as np
 import pytablewriter
 
+# Abstract
+print '# Abstract'
+print '> Recently, the problem of imbalanced data is the focus of intense research of machine learning community. Following work tries to utilize an approach of transforming the data space into another where classification task may become easier. Paper contains a proposition of a tool, based on a photographic metaphor to build a classifier ensemble, combined with a random subspace approach. It is a naturally naive parallel, insensitive to a sample size, robust to dimension increase, which allows a regularization of feature space, to reduce the impact of biased classes. The proposed approach was evaluated on the basis of the computer experiments carried out on the benchmark datasets.'
+
 csvArray = np.genfromtxt('csv/experiment_1_syntetic5f.csv', delimiter=',')
 print '# Hello!\n'
 
